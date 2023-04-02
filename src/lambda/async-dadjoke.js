f36c570102bf3b17b0e6d8a302236eccc5615aa5
vishal kumar <vishalraj42000@gmail.com>
2023-04-02 14:59:07 +0530

    changes

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -4,8 +4,8 @@
 import axios from "axios"
 export async function handler(event, context) {
   try {
-    const response = await axios.get("http://ip-api.com/json")
-    const data = response.data
+    // const response = await axios.get("http://ip-api.com/json")
+    const data = "Hello world"
     return {
       statusCode: 200,
       body: JSON.stringify({ msg: data })

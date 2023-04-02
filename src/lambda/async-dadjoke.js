8bf4c6cfcc9fd017c0da926a8b830832f59c02f4
vishal kumar <vishalraj42000@gmail.com>
2023-04-02 15:08:13 +0530

    changes

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -3,18 +3,19 @@
 
 import axios from "axios"
 export async function handler(event, context) {
-  try {
-    const response = await axios.get("http://ip-api.com/json")
-    const data = response.data
+  await axios.get("http://ip-api.com/json")
+  .then((resp) => {
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: data })
+      body: JSON.stringify({ msg: resp.data })
     }
-  } catch (err) {
+  })
+  
+  .catch ((err) => {
     console.log(err) // output to netlify function log
     return {
       statusCode: 500,
       body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
     }
-  }
+  })
 }
